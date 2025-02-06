dde5800e12653ce263cbf3b661b8ed769f439022
Bradley Nelson <vb7zl582@students.codeinstitute.net>
2025-02-06 18:38:51 +0000

    add show question function

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ nextButton.addEventListener('click', () => {
     currentQuestionIndex++
     setNextQuestion()
 })
+
+
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+}
